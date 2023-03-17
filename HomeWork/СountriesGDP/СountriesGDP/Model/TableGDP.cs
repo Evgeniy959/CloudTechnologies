0af65624b7c95b9ea2233900030fcafe0e48db1e
Pili_zr36 <77540303+Evgeniy959@users.noreply.github.com>
2023-03-17 11:31:07 +0700

    add

--- HomeWork/СountriesGDP/СountriesGDP/Model/TableGDP.cs
+++ HomeWork/СountriesGDP/СountriesGDP/Model/TableGDP.cs
@@ -1,9 +1,24 @@
-﻿namespace СountriesGDP.Model
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Xml.Linq;
+
+namespace СountriesGDP.Model
 {
+    [Table("Table_GDP_2022_year")]
     public class TableGDP
     {
         public int Id { get; set; }
+        public int Rating { get; set; }
         public string Country { get; set; }
-        public string Year { get; set; }
+        [Column("GDP,billion_$")]
+        public double VolumeGDP { get; set; }
+
+        public TableGDP() 
+        {
+            Id = default;
+            Rating = default;
+            Country = "";
+            VolumeGDP = default;
+        }
     }
 }
--- HomeWork/СountriesGDP/СountriesGDP/Program.cs
+++ HomeWork/СountriesGDP/СountriesGDP/Program.cs
@@ -1,6 +1,67 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
+using System.Diagnostics.Metrics;
+using ÑountriesGDP.Model;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<ApplicationDbContext>();
 var app = builder.Build();
+//TableGDP data1 = new TableGDP();
 
 app.MapGet("/", () => "Hello World!");
+app.MapGet("/all", async (ApplicationDbContext db) =>
+{
+    return await db.TableGDP.ToListAsync();
+});
+
+app.MapPost("/add", async (TableGDP data, ApplicationDbContext db) =>
+{
+    db.TableGDP.Add(data);
+    await db.SaveChangesAsync();
+    return data;
+});
+app.MapGet("/find", async (int id, ApplicationDbContext db) =>
+{
+    return await db.TableGDP.Where(x => x.Id == id).ToListAsync();
+});
+app.MapGet("/delete", async (int id, ApplicationDbContext db) =>
+{
+    TableGDP data = db.TableGDP.Find(id);
+    if (data != null)
+    {
+        db.TableGDP.Remove(data);
+        await db.SaveChangesAsync();
+    }
+    return await db.TableGDP.ToListAsync();
+});
+
+/*app.MapGet("/update", async (int rating, string country, string countryNew, double volumeGDP, ApplicationDbContext db) =>
+{
+    //TableGDP dataNew = db.TableGDP.Find(id);
+    //TableGDP dataNew = db.TableGDP.Where(x => x.Country == country).FirstOrDefault();
+    TableGDP dataNew = db.TableGDP.FirstOrDefault(x => x.Country == country);
+    if (dataNew != null)
+    {
+        dataNew.Rating = rating;
+        dataNew.Country = countryNew;
+        dataNew.VolumeGDP = volumeGDP;
+        await db.SaveChangesAsync();
+    }
+    return await db.TableGDP.ToListAsync();
+});*/
+app.MapPost("/update", async (TableGDP data, ApplicationDbContext db) =>
+{
+    TableGDP dataNew = db.TableGDP.Find(data.Id);
+    //TableGDP dataNew = db.TableGDP.FirstOrDefault(x => x.Country == data.Country);
+    if (data != null)
+    {
+        dataNew.Rating = data.Rating;
+        dataNew.Country = data.Country;
+        dataNew.VolumeGDP = data.VolumeGDP;
+        await db.SaveChangesAsync();
+    }
+    return await db.TableGDP.ToListAsync();
+});
 
 app.Run();
